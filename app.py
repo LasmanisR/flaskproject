c702be99e18b0e792b718ee4de1aeef7c80009f0
LasmanisR <lasmanis.rafaels@gmail.com>
2019-12-17 18:37:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import time
 import RPi.GPIO as GPIO
+import time
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
-while True:
-	GPIO.output(4,True)
-	time.sleep(1)
-	GPIO.output(4,False)
-	time.sleep(1)
+GPIO.setwarnings(False)
+GPIO.setup(4,GPIO.OUT)
+print "LED on"
+GPIO.output(4,GPIO.HIGH)
+time.sleep(1)
+print "LED off"
+GPIO.output(4,GPIO.LOW)
