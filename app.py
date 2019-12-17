0d81960f8b222f0fb227d619859a1cc12479f7f5
LasmanisR <lasmanis.rafaels@gmail.com>
2019-12-17 09:04:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,45 +3,33 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-ledRed = 13
-ledYellow= 19
-ledGreen= 26
-ledRedSts = 0
-ledYellowSts = 0
-ledGreenSts = 0
-GPIO.setup(ledRed, GPIO.OUT)
-GPIO.setup(ledYellow,GPIO.OUT)
-GPIO.setup(ledGreen, GPIO.OUT)
-GPIO.output(ledRed, GPIO.LOW)
-GPIO.output(ledYellow, GPIO.LOW)
-GPIO.output(ledGreen, GPIO.LOW)
+led = 0
+led2 = 0
+GPIO.setup(2, GPIO.OUT)
+GPIO.setup(3,GPIO.OUT)
+GPIO.output(2, GPIO.LOW)
+GPIO.output(3, GPIO.LOW)
 @app.route('/')
 def index():
-    ledRedSts = GPIO.input(ledRed)
-    ledYellowSts = GPIO.input(ledYellow)
-    ledGreenSts = GPIO.input(ledGreen)
-    templateData = { 'ledRed' : ledRedSts,
-    'ledYellow' : ledYellowSts,
-    'ledGreen' : ledGreenSts }
+    ledZSts = GPIO.input(2)
+    ledSSts = GPIO.input(3)
+    templateData = { 'ledZ' : led,
+    'ledS' : led2 }
     return render_template('index.html', **templateData)
 @app.route('/<devicename>/<action>')
 def do(deviceName, action):
-    if deviceName == "ledRed":
-        actuator = ledRed
-    if deviceName == "ledYellow":
-        actuator = ledYellow
-    if deviceName == "ledGreen":
-        actuator = ledGreen
+    if deviceName == "ledZ":
+        actuator = 2
+    if deviceName == "ledS":
+        actuator = 3
     if action == "on":
         GPIO.output(actuator, GPIO.HIGH)
     if action == "off":
         GPIO.output(actuator, GPIO.LOW)
-    ledRedSts = GPIO.input(ledRed)
-    ledYellowSts = GPIO.input(ledYellow)
-    ledGreenSts = GPIO.input(ledGreen)
-    templateData = { 'ledRed' : ledRedSts,
-    'ledYellow' : ledYellowSts,
-    'ledGreen' : ledGreenSts }
+    ledZSts = GPIO.input(2)
+    ledSSts = GPIO.input(3)
+    templateData = { 'ledZ' : 2,
+    'ledS' : 3 }
     return render_template('index.html', **templateData )
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', debug=True)
