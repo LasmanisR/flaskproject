afd8de5c2070b498598acc0b253d8fedc7429357
LasmanisR <lasmanis.rafaels@gmail.com>
2019-12-17 09:13:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,46 @@
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-led = 0
-led2 = 0
-GPIO.setup(2, GPIO.OUT)
-GPIO.setup(3,GPIO.OUT)
-GPIO.output(2, GPIO.LOW)
-GPIO.output(3, GPIO.LOW)
-@app.route('/')
-def index():
-    ledSts = GPIO.input(2)
-    led2Sts = GPIO.input(3)
-    templateData = { 'led' : led,
-    'led2' : led2 }
-    return render_template('index.html', **templateData)
-@app.route('/<devicename>/<action>')
-def do(deviceName, action):
-    if deviceName == "led":
-        actuator = 2
-    if deviceName == "led2":
-        actuator = 3
-    if action == "on":
-        GPIO.output(actuator, GPIO.HIGH)
-    if action == "off":
-        GPIO.output(actuator, GPIO.LOW)
-    ledSts = GPIO.input(2)
-    led2Sts = GPIO.input(3)
-    templateData = { 'led' : 2,
-    'led2' : 3 }
-    return render_template('index.html', **templateData )
+
+pins = {
+   2 : {'name' : 'GPIO 2', 'state' : GPIO.LOW},
+   3 : {'name' : 'GPIO 3', 'state' : GPIO.LOW}
+   }
+
+for pin in pins:
+   GPIO.setup(pin, GPIO.OUT)
+   GPIO.output(pin, GPIO.LOW)
+
+@app.route("/")
+def main():
+   for pin in pins:
+      pins[pin]['state'] = GPIO.input(pin)
+   templateData = {
+      'pins' : pins
+      }
+   return render_template('main.html', **templateData)
+
+@app.route("/<changePin>/<action>")
+def action(changePin, action):
+   changePin = int(changePin)
+   deviceName = pins[changePin]['name']
+   if action == "on":
+      GPIO.output(changePin, GPIO.HIGH)
+      message = "Turned " + deviceName + " on."
+   if action == "off":
+      GPIO.output(changePin, GPIO.LOW)
+      message = "Turned " + deviceName + " off."
+
+   for pin in pins:
+      pins[pin]['state'] = GPIO.input(pin)
+
+   templateData = {
+      'pins' : pins
+   }
+
+   return render_template('main.html', **templateData)
+
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', debug=True)
+   app.run(host='0.0.0.0', debug=True)
