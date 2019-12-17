dfe5afaefa2ef92bb9de0cd1560ab21c2c27e045
LasmanisR <lasmanis.rafaels@gmail.com>
2019-12-17 20:25:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
-# main.py
-import RPi.GPIO as GPIO
+Learn more or give us feedback
 from flask import Flask
 app = Flask(__name__)
+import os
 
-ledPin = 21
-GPIO.setmode(GPIO.BCM)
 
-@app.route('/on')
-def turn_led_on():
-    GPIO.output(ledPin, GPIO.HIGH)
-    return "OK"
+@app.route("/gpio21/direction/out")
+def gpioDirectionRead(number):
+	cmd = 'cat /sys/class/gpio/gpio{21}/direction'.format(number)
+	direction = os.popen(cmd).read()
+	return "gpio{21} direction == {out}".format(number, direction)
 
-@app.route('/off')
-def turn_led_off():
-    GPIO.output(ledPin, GPIO.LOW)
-    return "OK"
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+@app.route("/gpio21/value/off")
+def gpioValueRead(number):
+	cmd = 'cat /sys/class/gpio/gpio{21}/value'.format(number)
+	state = os.popen(cmd).read()
+	return "gpio{21} value == {LOW}".format(number, state)
+
+@app.route("/gpio21/value/on")
+def gpioValueSet(number, state):
+	cmd = 'echo {} > /sys/class/gpio/gpio{21}/value'.format(state, number)
+	os.system(cmd)
+	return "gpio{21} value set {HIGH}".format(number, state)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
