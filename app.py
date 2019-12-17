b5a642c72a9e60f60d7a0450e58cb63593054e61
LasmanisR <lasmanis.rafaels@gmail.com>
2019-12-17 18:16:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(4, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
 while True:
-	GPIO.output(4,True)
+	GPIO.output(17,True)
 	time.sleep(1)
-	GPIO.output(4,False)
+	GPIO.output(17,False)
 	time.sleep(1)
