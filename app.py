a507ac79b7214feb8c8852e7a971b8d7c7e16f36
LasmanisR <lasmanis.rafaels@gmail.com>
2019-12-17 09:07:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,25 +11,25 @@ GPIO.output(2, GPIO.LOW)
 GPIO.output(3, GPIO.LOW)
 @app.route('/')
 def index():
-    ledZSts = GPIO.input(2)
-    ledSSts = GPIO.input(3)
-    templateData = { 'ledZ' : led,
-    'ledS' : led2 }
+    ledSts = GPIO.input(2)
+    led2Sts = GPIO.input(3)
+    templateData = { 'led' : led,
+    'led2' : led2 }
     return render_template('index.html', **templateData)
 @app.route('/<devicename>/<action>')
 def do(deviceName, action):
-    if deviceName == "ledZ":
+    if deviceName == "led":
         actuator = 2
-    if deviceName == "ledS":
+    if deviceName == "led2":
         actuator = 3
     if action == "on":
         GPIO.output(actuator, GPIO.HIGH)
     if action == "off":
         GPIO.output(actuator, GPIO.LOW)
-    ledZSts = GPIO.input(2)
-    ledSSts = GPIO.input(3)
-    templateData = { 'ledZ' : 2,
-    'ledS' : 3 }
+    ledSts = GPIO.input(2)
+    led2Sts = GPIO.input(3)
+    templateData = { 'led' : 2,
+    'led2' : 3 }
     return render_template('index.html', **templateData )
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', debug=True)
