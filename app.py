5f704a1735d8df351dacb938f0059a238cb53b89
LasmanisR <lasmanis.rafaels@gmail.com>
2019-12-17 20:28:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 import os
 
-
-@app.route("/gpio21/direction/out")
-def gpioDirectionRead(number):
-	cmd = 'cat /sys/class/gpio/gpio{21}/direction'.format(number)
-	direction = os.popen(cmd).read()
-	return "gpio{21} direction == {out}".format(number, direction)
-
-
-@app.route("/gpio21/value/off")
-def gpioValueRead(number):
-	cmd = 'cat /sys/class/gpio/gpio{21}/value'.format(number)
-	state = os.popen(cmd).read()
-	return "gpio{21} value == {LOW}".format(number, state)
-
 @app.route("/gpio21/value/on")
-def gpioValueSet(number, state):
+def gpioValueSet(21, HIGH):
 	cmd = 'echo {} > /sys/class/gpio/gpio{21}/value'.format(state, number)
 	os.system(cmd)
 	return "gpio{21} value set {HIGH}".format(number, state)
